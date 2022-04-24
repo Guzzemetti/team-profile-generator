b8726c187894a46fd3878428e2a22fb734dfedad
John Guzzetta <johnguzzettaiv@gmail.com>
2022-04-24 18:50:43 -0400

    Added in framework for HTML into html.js.

---
+++ html.js
@@ -0,0 +1,122 @@
+
+
+
+
+// function generateHtml(data){
+
+//     return `
+//     <!doctype html>
+// <html lang="en">
+
+// <head>
+//     <!-- Required meta tags -->
+//     <meta charset="utf-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+//     <!-- Bootstrap CSS -->
+//     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+//         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+//     <title>Team Profile</title>
+// </head>
+
+// <body>
+//     <header class="container-fluid" style="height: 10rem; padding: 3rem; border-bottom: solid black;">
+//         <h1 class="text-center">My Team</h1>
+//     </header>
+//     <div class="container">
+//         <div class="row">
+//             <div class="col">
+//                 <!-- Left Column -->
+//                 <div class="text-center" style="border: solid black; width: 18rem; margin: 2rem;">
+//                     <h2>Intern</h2>
+//                 </div>
+//                 <div class="card text-center" style="width: 18rem; margin: 2rem;">
+//                     <div class="card-body">
+//                         <h5 class="card-title">Employee Name</h5>
+//                         <h6 class="card-subtitle mb-2 text-muted">Employee Role</h6>
+//                         <ul class="list-group">
+//                             <li class="list-group-item">Employee ID</li>
+//                             <li class="list-group-item">Employee Email</li>
+//                             <li class="list-group-item">School/Github/Office Number</li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//                 <div class="card text-center" style="width: 18rem; margin: 2rem;">
+//                     <div class="card-body">
+//                         <h5 class="card-title">Card title</h5>
+//                         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//                         <ul class="list-group">
+//                             <li class="list-group-item">Employee ID</li>
+//                             <li class="list-group-item">Employee Email</li>
+//                             <li class="list-group-item">School/Github/Office Number</li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div class="col">
+//                 <!-- Center Column -->
+//                 <div class="text-center" style="border: solid black; width: 18rem; margin: 2rem;">
+//                     <h2>Engineer</h2>
+//                 </div>
+//                 <div class="card text-center" style="width: 18rem; margin: 2rem;">
+//                     <div class="card-body">
+//                         <h5 class="card-title">Card title</h5>
+//                         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//                         <ul class="list-group">
+//                             <li class="list-group-item">Employee ID</li>
+//                             <li class="list-group-item">Employee Email</li>
+//                             <li class="list-group-item">School/Github/Office Number</li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//                 <div class="card text-center" style="width: 18rem; margin: 2rem;">
+//                     <div class="card-body">
+//                         <h5 class="card-title">Card title</h5>
+//                         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//                         <ul class="list-group">
+//                             <li class="list-group-item">Employee ID</li>
+//                             <li class="list-group-item">Employee Email</li>
+//                             <li class="list-group-item">School/Github/Office Number</li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div class="col">
+//                 <!-- Right Column -->
+//                 <div class="text-center" style="border: solid black; width: 18rem; margin: 2rem;">
+//                     <h2>Manager</h2>
+//                 </div>
+//                 <div class="card text-center" style="width: 18rem; margin: 2rem;">
+//                     <div class="card-body">
+//                         <h5 class="card-title">Card title</h5>
+//                         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//                         <ul class="list-group">
+//                             <li class="list-group-item">Employee ID</li>
+//                             <li class="list-group-item">Employee Email</li>
+//                             <li class="list-group-item">School/Github/Office Number</li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//                 <div class="card text-center" style="width: 18rem; margin: 2rem;">
+//                     <div class="card-body">
+//                         <h5 class="card-title">Card title</h5>
+//                         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//                         <ul class="list-group">
+//                             <li class="list-group-item">Employee ID</li>
+//                             <li class="list-group-item">Employee Email</li>
+//                             <li class="list-group-item">School/Github/Office Number</li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+//         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+//         crossorigin="anonymous"></script>
+// </body>
+
+// </html>
+//     `
+// }
