689ef29b15806d8a213d23d3c29bd1553e8df179
John Guzzetta <johnguzzettaiv@gmail.com>
2022-04-25 22:59:10 -0400

    Commented out index.js and added comments for future fixes. Began employee tests, verified Employee class is established correctly.

--- index.js
+++ index.js
@@ -1,92 +1,96 @@
-const inquirer = require("inquirer")
-// Initial prompts that apply to all employees
-const employeeInfoRequest = [
-    {
-        type: "input",
-        name: "empName",
-        message: "Please enter the name of this employee"
-    },
-    {
-        type: "list",
-        name: "employeeRole",
-        message: "Please select their role",
-        choices: ["Intern", "Engineer", "Manager"]
-    },
-    {
-        type: "input",
-        name: "employeeEmail",
-        message: "Please enter their email address"
-    },
-    {
-        type: "input",
-        name: "employeeID",
-        message: "Please enter their employee ID"
-    }
-];
-// Prompt for engineers
-const githubRequest = [
-    {
-        type: "input",
-        name: "githubLink",
-        message: "Please enter the URL to your GitHub Profile"
-    },
-    {
-        type: "confirm",
-        name: "addEmployee",
-        message: "Would you like to add another employee?"
-    }
-];
-// Prompt for interns
-const schoolRequest = [
-    {
-        type: "input",
-        name: "schoolName",
-        message: "Please enter the name of the School they attend"
-    },
-    {
-        type: "confirm",
-        name: "addEmployee",
-        message: "Would you like to add another employee?"
-    }
-];
-// Prompt for manager
-const officeNumberReq = [
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "Please enter their office phone number"
-    },
-    {
-        type: "confirm",
-        name: "addEmployee",
-        message: "Would you like to add another employee?"
-    }
-]
-// Function to prompt user to fill out the employeeInfoRequest request
-function roleInfo(data) {
-    inquirer.prompt(employeeInfoRequest)
-        // Depending on what role the employee falls under, they get prompted with an additional question specific to their role
-        .then(data => {
-            if (data.employeeRole === "Engineer") {
-                inquirer.prompt(githubRequest);
-            }
-            if (data.employeeRole === "Intern") {
-                inquirer.prompt(schoolRequest);
-            }
-            if (data.employeeRole === "Manager") {
-                inquirer.prompt(officeNumberReq);
-            }  
-        })
-        return data;
-};
-roleInfo();
+// TODOS
+// Need to re-write prompt arrays
+// Make one array for Manager questions, this will run once
+// Create a second array for employee prompts, using the "when" function below to display certain prompts only when the specific role is determined
+// use the blog linked to loop through the array repeatedly
 
 
-// async function addEmployee(data) {
-//     await roleInfo(data)
-//     .then(data => {
-//         if (data.addEmployee === true) {
-//             inquirer.prompt(employeeInfoRequest);
-// }});
-// }
 
+
+
+
+// const write = require("fs");
+// const inquirer = require("inquirer");
+
+// // Initial prompts that apply to all employees
+// const employeeInfoRequest = [
+//     {
+//         type: "input",
+//         name: "empName",
+//         message: "Please enter the name of this employee"
+//     },
+//     {
+//         type: "list",
+//         name: "employeeRole",
+//         message: "Please select their role",
+//         choices: ["Intern", "Engineer", "Manager"]
+//     },
+//     {
+//         type: "input",
+//         name: "employeeEmail",
+//         message: "Please enter their email address"
+//     },
+//     {
+//         type: "input",
+//         name: "employeeID",
+//         message: "Please enter their employee ID"
+//     }
+//     {
+//         type: "input",
+//         name: "githubLink",
+//         message: "Please enter the URL to your GitHub Profile",
+//         when(answer){
+//             return answer.employeeRole === "Engineer"
+//         }
+//     },
+//     {
+//         type: "confirm",
+//         name: "addEmployee",
+//         message: "Would you like to add another employee?"
+//     }
+// ];
+// // Prompt for interns
+// const schoolRequest = [
+//     {
+//         type: "input",
+//         name: "schoolName",
+//         message: "Please enter the name of the School they attend"
+//     },
+//     {
+//         type: "confirm",
+//         name: "addEmployee",
+//         message: "Would you like to add another employee?"
+//     }
+// ];
+// // Prompt for manager
+// const officeNumberReq = [
+//     {
+//         type: "input",
+//         name: "officeNumber",
+//         message: "Please enter their office phone number"
+//     },
+//     {
+//         type: "confirm",
+//         name: "addEmployee",
+//         message: "Would you like to add another employee?"
+//     }
+// ]
+// // Function to prompt user to fill out the employeeInfoRequest request
+// function roleInfo(data) {
+//     inquirer.prompt(employeeInfoRequest)
+//         // Depending on what role the employee falls under, they get prompted with an additional question specific to their role
+//         .then(data => {
+//             if (data.employeeRole === "Engineer") {
+//                 inquirer.prompt(githubRequest);
+//             }
+//             if (data.employeeRole === "Intern") {
+//                 inquirer.prompt(schoolRequest);
+//             }
+//             if (data.employeeRole === "Manager") {
+//                 inquirer.prompt(officeNumberReq);
+//             }  
+//         })
+//         .then((data) => {write(data)})
+// };
+
+// roleInfo();
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,9 +1,29 @@
 const Employee = require("../lib/employee");
 
-
+// Establishes that Employee is an object
 describe("Employee", () => {
-    it("Baseline employee class"), () => {
-        const e =new Employee();
-        expect(new Employee(e)).toBe("object");
-    }
+    it("Baseline employee class", () => {
+        const e = new Employee();
+        expect(typeof(e)).toBe("object");
+    })
 });
+// Establishes the employee name
+it("Sets the employee name", () => {
+    const name = "John";
+    const e = new Employee("John");
+    expect(e.employeeName).toBe(name);
+});
+
+// Establishes the name and ID
+it("Sets the employee ID", () => {
+    const id = 12;
+    const e = new Employee("John", 12);
+    expect(e.id).toBe(id)
+})
+
+// Establishes the email address
+it("Sets the employee email address", () => {
+    const email = "jGuzz@yahoo.com";
+    const e = new Employee("John", 12, "jGuzz@yahoo.com");
+    expect(e.email).toBe(email)
+})
